7261beae6895d6f77f3fa41a0c5021a1e45f56e9
Joy Offiong <blaqkly@gmail.com>
2023-04-15 20:54:34 +0100

    map

--- app.js
+++ app.js
@@ -6,3 +6,28 @@ btn.addEventListener('click', () => {
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
 })
+
+const team =[
+    { img:'./img/pm.png', title:'Marketing Manager'},
+    { img:'./img/mh.png', title:'Brand Manager'},
+    { img:'./img/hos.png', title:'Brand Manager'}
+]
+
+let output = document.getElementById("output");
+output.innerHTML = team.map((teamMembers)=>{
+    const {img, title}= teamMembers
+    return(
+        `
+        <div class="md:w-[32%] bg-black ">
+        <div class=" rounded-full h-[500px] text-center" style="backgroundColor: black;">
+            <img src=${img} alt="" class="object-cover" 
+                style='height:350px; object-fit: cover; '>
+        </div>
+        <p class="font-[500] text-[23px] leading-[23px] text-[#8b8b8b] my-[20px] text-center">${title}
+        </p>
+    </div>
+        `
+    )
+})
+
+output.appendChild(eachOutput);
